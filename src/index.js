d782b9081e7054873febbcf9c8f921871a213e4a
Hamed_Morady <165544770+Hamed-Morady@users.noreply.github.com>
2025-05-13 11:38:13 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -334,14 +334,35 @@ function Cart({
                 onClick={() => minus(cart.id)}
                 className="btn-counter minus"
               >
-                <span className="">-</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="2"
+                  fill="none"
+                  viewBox="0 0 10 2"
+                  className="icon-minus-custom"
+                >
+                  <path fill="currentColor" d="M0 .375h10v1.25H0V.375Z" />
+                </svg>
               </button>
               <span className="counter-count">{quantity[cart.id] || 0}</span>
               <button
                 onClick={() => plusHandler(cart.id)}
                 className="btn-counter plus"
               >
-                <span className="">+</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  height="10"
+                  fill="none"
+                  viewBox="0 0 10 10"
+                  className="icon-minus-custom"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+                  />
+                </svg>
               </button>
             </div>
           )}
